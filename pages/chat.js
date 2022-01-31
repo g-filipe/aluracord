0346fd31fcb80ba22d011f317d8fc7ca959385b0
g-filipe <galvao.s.filipe@gmail.com>
2022-01-30 23:27:30 -0300

    Implementação do botão enviar | Reconhecimento do Shift+Enter como quebra de linha - Problema: Fazer o texto ser submetido com a quebra de linha | Alterar o comportamento padrão de scroll lateral (Implementar quebra de texto automática no textarea | Estilizar o scroll no desktop

--- pages/chat.js
+++ pages/chat.js
@@ -2,172 +2,197 @@ import { Box, Text, TextField, Image, Button } from '@skynexui/components';
 import React from 'react';
 import appConfig from '../config.json';
 
-export default function chatPage(){
-      const [mensagem, setMensagem] = React.useState('');
-      const [listaDeMensagens, setListaDeMensagens] = React.useState([])
 
-      return (
+export default function ChatPage() {
+    const [mensagem, setMensagem] = React.useState('');
+    const [listaDeMensagens, setListaDeMensagens] = React.useState([]);
+
+    function handleNovamensagem(novaMensagem) {
+        const mensagem = {
+            id: listaDeMensagens.length + 1,
+            de: 'Filipe Galvão',
+            texto: novaMensagem,
+        }
+
+        setListaDeMensagens([
+            mensagem,
+            ...listaDeMensagens,
+        ]);
+
+        setMensagem('');
+    }
+
+    return (
+        <Box
+            styleSheet={{
+                display: 'flex', alignItems: 'center', justifyContent: 'center',
+                backgroundColor: appConfig.theme.colors.primary[500],
+                backgroundImage: `url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)`,
+                backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
+                color: appConfig.theme.colors.neutrals['000']
+            }}
+        >
             <Box
                 styleSheet={{
-                  display: 'flex', alignItems: 'center', justifyContent: 'center',
-                  backgroundColor: appConfig.theme.colors.primary[500],
-                  backgroundImage: `url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)`,
-                  backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
-                  color: appConfig.theme.colors.neutrals['000']
-                }}
-          >
-              <Box
-                  styleSheet={{
                     display: 'flex',
                     flexDirection: 'column',
                     flex: 1,
                     boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
-                    borderRadius: '5px', 
+                    borderRadius: '5px',
                     backgroundColor: appConfig.theme.colors.neutrals[700],
                     height: '100%',
                     maxWidth: '95%',
                     maxHeight: '95vh',
                     padding: '32px',
-                  }}
-              >
+                }}
+            >
                 <Header />
                 <Box
                     styleSheet={{
-                      position: 'relative',
-                      display: 'flex',
-                      flex: 1,
-                      height: '80%',
-                      backgroundColor: appConfig.theme.colors.neutrals[600],
-                      flexDirection: 'column',
-                      borderRadius: '5px', 
-                      padding: '16px',
+                        position: 'relative',
+                        display: 'flex',
+                        flex: 1,
+                        height: '80%',
+                        backgroundColor: appConfig.theme.colors.neutrals[600],
+                        flexDirection: 'column',
+                        borderRadius: '5px',
+                        padding: '16px',
                     }}
                 >
-                   <MessageList />
-                   {listaDeMensagens.map((mensagemAtual) =>{
-                     return (
-                       <li>
-                          {mensagemAtual}
-                       </li>
-                     )
-                   })}
+                    <MessageList mensagens={listaDeMensagens} />
 
-                  <Box
-                      as="form"
-                      styleSheet={{
-                        display: 'flex',
-                        alignItems: 'center',
-                    }}
-                  >
-                    <TextField 
-                        value={mensagem}
-                        onChange={(event) =>{
-                            const valor = event.target.value;
-                            setMensagem(valor);
-                        }}
-                        onKeyPress={(event) =>{
-                          if(event.key === 'Enter'){
-                              setListaDeMensagens([
-                                ...listaDeMensagens,
-                                 mensagem
-                              ]);
-                            setMensagem('');
-                          }
-
-                      }}
-                        placeholder="Insira sua mensagem aqui..."
-                        type= "textarea"
+                    <Box
+                        as="form"
                         styleSheet={{
-                            width: '100%',
-                            border: '0',
-                            resize: 'none',
-                            borderRadius: '5px',
-                            padding: '6px 8px', 
-                            backgroundColor: appConfig.theme.colors.neutrals[800],
-                            marginRight: '12px',
-                            color: appConfig.theme.colors.neutrals[200],
+                            display: 'flex',
+                            alignItems: 'center',
                         }}
-                    />
+                    >
+                        <TextField
+                            value={mensagem}
+                            onChange={(event) => {
+                                const valor = event.target.value;
+                                setMensagem(valor);
+                            }}
+                            onKeyPress={(event) => {
+                                if (event.key === 'Enter' && !event.shiftKey) {
+                                    event.preventDefault();
+                                    handleNovamensagem(mensagem);
+                                } 
+                            }}
+                            placeholder="Insira sua mensagem aqui..."
+                            type="textarea"
+                            styleSheet={{
+                                width: '100%',
+                                border: '0',
+                                resize: 'none',
+                                borderRadius: '5px',
+                                padding: '6px 8px',
+                                backgroundColor: appConfig.theme.colors.neutrals[800],
+                                marginRight: '12px',
+                                color: appConfig.theme.colors.neutrals[200],
+                            }}
+                        />
+                            <Button
+                            type='submit' 
+                            label='Enviar'
+                            variant='tertiary'
+                            colorVariant='neutral'
+
+                            onClick={(event) => {
+                                  event.preventDefault();
+                                  handleNovamensagem(mensagem);
+                            }
+                          }
+                            styleSheet={{
+                                marginRight: '15px',
+                                marginBottom: '10px',
+                                color: appConfig.theme.colors.neutrals[200],
+                            }}
+                            />
+                    </Box>
                 </Box>
             </Box>
         </Box>
-    </Box>
-  )
-}
-
-function Header(){
-    return (
-      <>
-          <Box styleSheet={{ width: '100%', marginBottom: '16px', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }} >
-              <Text variant='heading5'>
-                  Chat
-              </Text>
-              <Button
-                  variant='tertiary'
-                  colorVariant='neutral'
-                  label='Logout'
-                  href="/"
-              />
-          </Box>
-      </>
     )
-  }
-function MessageList(){
-  return (
-      <Box 
-          tag="ul"
-          styleSheet={{
-            overflow: 'scroll',
-            display: 'flex',
-            flexDirection: 'column-reverse',
-            flex: 1,
-            color: appConfig.theme.colors.neutrals["000"],
-            marginBottom: '16px',
-          }}
-          
-      >  
-          <Text 
-              tag="li"
-              styleSheet={{
-                  borderRadius: '5px',
-                  padding: '6px',
-                  marginBottom: '12px',
-                  hover: {
-                    backgroundColor: appConfig.theme.colors.neutrals[700],
-                  }
-              }}
-          >  
-                <Box
-                    styleSheet={{
-                        marginBottom: '8px',
-                    }}
-                >  
-                    <Image
-                        styleSheet={{
-                            width: '20px',
-                            height: '20px',
-                            borderRadius: '50%',
-                            display: 'inline-block',
-                            marginRight: '8px',
-                        }}
-                        src={`https://github.com/g-filipe.png`}
-                    />
-                    <Text tag="strong">
-                        g-filipe
-                    </Text>
+}
+function Header() {
+    return (
+        <>
+            <Box styleSheet={{ width: '100%', marginBottom: '16px', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }} >
+                <Text variant='heading5'>
+                    Chat
+                </Text>
+                <Button
+                    variant='tertiary'
+                    colorVariant='neutral'
+                    label='Logout'
+                    href="/"
+                />
+            </Box>
+        </>
+    )
+}
+function MessageList(props) {
+    return (
+        <Box
+            tag="ul"
+            styleSheet={{
+                overflow: 'auto', 
+                display: 'flex',
+                flexDirection: 'column-reverse',
+                flex: 1,
+                color: appConfig.theme.colors.neutrals["000"],
+                marginBottom: '16px',
+            }}
+        >
+            {props.mensagens.map((mensagem) => {
+                return (
                     <Text
+                        key={mensagem.id}
+                        tag="li"
                         styleSheet={{
-                          fontSize: '10px',
-                          marginLeft: '8px',
-                          color: appConfig.theme.colors.neutrals[300],
+                            borderRadius: '5px',
+                            padding: '6px',
+                            marginBottom: '12px',
+                            hover: {
+                                backgroundColor: appConfig.theme.colors.neutrals[700],
+                            }
                         }}
-                        tag="span"
                     >
-                        {(new Date().toLocaleDateString())}
+                        <Box
+                            styleSheet={{
+                                marginBottom: '8px',
+                            }}
+                        >
+                            <Image
+                                styleSheet={{
+                                    width: '20px',
+                                    height: '20px',
+                                    borderRadius: '50%',
+                                    display: 'inline-block',
+                                    marginRight: '8px',
+                                }}
+                                src={`https://github.com/g-filipe.png`}
+                            />
+                            <Text tag="strong">
+                                {mensagem.de}
+                            </Text>
+                            <Text
+                                styleSheet={{
+                                    fontSize: '10px',
+                                    marginLeft: '8px',
+                                    color: appConfig.theme.colors.neutrals[300],
+                                }}
+                                tag="span"
+                            >
+                                {(new Date().toLocaleDateString())}
+                            </Text>
+                        </Box>
+                        {mensagem.texto}
                     </Text>
-                </Box>
-                Mensagem
-            </Text>
-      </Box>
-  )
+                );
+            })}
+        </Box>
+    )
 }
